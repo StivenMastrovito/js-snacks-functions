531639bd047ed79216bd2af34ed9dee9b51ca764
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-11-04 15:15:13 +0100

    feat: filtra iniziali

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -1,12 +1,23 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera
+ e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function filtraIniziali(array, lettera) {
+    const filtrato = [];
+    for(let i = 0; i < array.length; i++){
+        if(array[i][0] === lettera){
+            filtrato.push(array[i])
+        }
+    }    
+    return filtrato;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+
+console.log(filtraIniziali(names, "A"));
 
 
 
