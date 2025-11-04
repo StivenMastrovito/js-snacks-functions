0e60cc22cae8f0efcf56c902d0d3946071d63295
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-11-04 15:30:15 +0100

    feat: saluti

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,15 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
+/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un
+ saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
+const name = 'Giovanni';
 
 
 // Dichiara la funzione qui.
-
+const saluto = (nome) => "Ciao " + nome;
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(name));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
