82f0a8a7cf96ed0450f3fc0aa1ec0a6581c34438
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-11-04 15:34:42 +0100

    feat: bonus

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -1,4 +1,5 @@
-/* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito. 
+/* Scrivi una funzione che accetti una stringa contenente un nome e
+restituisca un saluto seguito dal nome fornito. 
 Il saluto deve essere :
 buongiorno se è mattina (fino alle 13), 
 buon pomeriggio se è pomeriggio (fino alle 17) 
@@ -10,9 +11,21 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function saluto(nome){
+    const ora = new Date();
+    if(ora.getHours() <= 13){
+        return "Buongiorno " + nome;
+    }else if(ora.getHours() > 17){
+        return "Buonasera " + nome;
+    }else{
+        return "Buon pomeriggio " + nome;
+    }
+    
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(saluto(name));
 
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
