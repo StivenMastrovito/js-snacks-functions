4751fd7f81ae8be40e195156ec544bc1cc4f0795
StivenMastrovito <stivenmastrovito6@gmail.com>
2025-11-04 15:10:26 +0100

    feat: iniziali

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,21 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca
+ nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function iniziali(array){
+    const arrayIniziali = [];
+    for(let i = 0; i < array.length; i++){
+        arrayIniziali.push(array[i][0]);
+    }
+    return arrayIniziali;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(iniziali(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
